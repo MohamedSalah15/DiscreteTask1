e440a54619223c5e727764fe6d18d887867cd387
MohamedSalah15 <121621712+MohamedSalah15@users.noreply.github.com>
2022-12-29 16:17:27 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("First Number");
+            int Start = int.Parse(Console.ReadLine());
+            Console.WriteLine("Second Number");
+            int End = int.Parse(Console.ReadLine());
+            int Sum = 0;
+            for (int k = Start; k <= End; k++) {
+                for (int xo = 1; xo <= k; xo++){
+                   if(k%xo==0)
+                    Sum++;
+                }
+                if(Sum==2){
+                    Console.WriteLine(k);
+                    Sum=0;
+                }else{
+                    Sum=0;
+                   continue;
+            }           
+               }      
+            }
+            
+
+       
+            
+
+
+        }
+    }
+
